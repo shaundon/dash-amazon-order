71d1588fcdd7fac802fbdd22f710bd3eb62914c2
Shaun Donnelly <shaun.donnelly@ensighten.com>
2016-11-25 17:21:09 +0000

    RGH

--- order.js
+++ order.js
@@ -7,47 +7,75 @@ const productId = config.amazon.productId;
 
 // Required for cookies to work.
 // http://stackoverflow.com/a/15438666/1011161
+phantom.enableCookies = true;
 casper.userAgent('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)');
 
 const AMAZON_URL = 'https://amazon.co.uk/';
 const LOGIN_FORM_SELECTOR = 'form[name="signIn"]';
-const ONE_CLICK_ENABLE_BUTTON = 'a.oneClickSignInLink';
-const ONE_CLICK_PURCHASE_BUTTON = 'input#oneClickBuyButton';
-const SUCCESS_SELECTOR = 'div#thank-you-header';
+const ENABLE_ONE_CLICK_BUTTON = 'a.oneClickSignInLink';
+const BUY_WITH_ONE_CLICK_BUTTON = 'input#oneClickBuyButton';
+const THANK_YOU_MESSAGE = 'div#thank-you-center';
 
-// Go to product page.
-casper.start(AMAZON_URL + 'dp/' + productId, function() {
-  this.echo('Loaded the product page.');
+// Go to orders page first to get a sign in prompt.
+casper.start(AMAZON_URL + 'orders', function() {
+  this.waitForSelector(LOGIN_FORM_SELECTOR);
 });
 
-casper.waitForSelector(ONE_CLICK_ENABLE_BUTTON);
-
-// Click the 'buy with one click' button.
+// Fill in the form.
 casper.then(function() {
-  this.echo('Clicking buy button');
-  this.click(ONE_CLICK_ENABLE_BUTTON);
-});
-
-// Login form appears.
-casper.waitForSelector(LOGIN_FORM_SELECTOR, function() {
   this.echo('Amazon login page loaded. Signing in as ' + email);
   this.fill(LOGIN_FORM_SELECTOR, {
     email: email,
     password: password
   }, true);
 });
-//
-// // Wait for the form to be gone.
-// casper.waitWhileSelector(LOGIN_FORM_SELECTOR, function(){
-//   this.echo('Signed in!');
-// });
-//
-// // Buy it!
-// casper.click(ONE_CLICK_PURCHASE_BUTTON);
-//
-// casper.waitForSelector(SUCCESS_SELECTOR, function() {
-//   casper.echo('Purchased!');
-//   this.capture('screenshot.png');
+
+// Wait for the form to be gone.
+casper.waitWhileSelector(LOGIN_FORM_SELECTOR, function() {
+  this.echo('Signed in!');
+});
+
+// Go to the product page.
+casper.thenOpen(AMAZON_URL + 'dp/' + productId, function() {
+  this.echo('Navigated to product page (' + AMAZON_URL + 'dp/' + productId + ')');
+  this.capture('screenshot.png', {
+    top: 0,
+    left: 0,
+    width: 1200,
+    height: 800
+  });
+});
+
+casper.then(function() {
+  this.echo('Enabling one click for this browser.');
+  this.click(ENABLE_ONE_CLICK_BUTTON);
+});
+
+casper.then(function() {
+  this.capture('screenshot1.png', {
+    top: 0,
+    left: 0,
+    width: 1200,
+    height: 800
+  });
+})
+
+casper.waitForSelector(BUY_WITH_ONE_CLICK_BUTTON, function() {
+  this.click(BUY_WITH_ONE_CLICK_BUTTON);
+});
+
+casper.waitForSelector(THANK_YOU_MESSAGE, function() {
+  this.capture('screenshot1.png', {
+    top: 0,
+    left: 0,
+    width: 1200,
+    height: 800
+  });
+  this.echo('Ordered successfully!');
+});
+
+// casper.onError(function() {
+//   this.echo('Something went wrong.');
 // });
 
 casper.run();
